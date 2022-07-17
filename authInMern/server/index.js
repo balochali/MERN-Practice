0d4cf997fa6c20fc37dc5a385d4cf74d9f13e06e
Ali Baloch <ali.albaloshi2000@gmail.com>
2022-07-18 02:40:54 +0500

    Sign UP Component React Added

--- authInMern/server/index.js
+++ authInMern/server/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const connection = require("./db")
+const userRoutes = require('./routes/user')
+const authRoutes = require('./routes/auth')
 
 //database connection 
 connection();
@@ -11,6 +13,12 @@ connection();
 //middlewares
 app.use(express.json())
 app.use(cors())
+
+
+
+//routes 
+app.use("api/users", userRoutes);
+app.use("api/auth", authRoutes);
 
 const port = process.env.PORT || 8080
 
